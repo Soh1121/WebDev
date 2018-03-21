55793d30052ae362cc4bd3f8b9cc2fccf9e8301e
Soh1121 <satou.shg@gmail.com>
2018-03-22 00:59:24 +0900

    【Variables 変数と値】数値の場合。計算結果も適切に処理してくれる。
    セクション8 レクチャー107受講中

--- 8-phpstudy/1-index.php
+++ 8-phpstudy/1-index.php
@@ -8,4 +8,12 @@ echo "<p>私の名前は、".$name."です。</p>";
 
 echo "<p>私の名前は、${name}です。";
 
+$myNumber = 45;
+
+echo "<p>番号：${myNumber}</p>";
+
+$calc = $myNumber * 31 / 97 + 4;
+
+echo "<p>計算結果：${calc}";
+
 ?>
