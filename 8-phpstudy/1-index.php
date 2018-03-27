176e5828df070f5bc50b09d9f42f10649bad8b82
Soh1121 <satou.shg@gmail.com>
2018-03-27 10:58:56 +0900

    【If Statement if文（if else）】比較演算子”もしくは”の学習
    セクション8 レクチャー109受講終了

--- 8-phpstudy/1-index.php
+++ 8-phpstudy/1-index.php
@@ -7,7 +7,8 @@ $role = "admin";
 $position = "manager";
 
 // if($role == "admin"){
-if($role == "admin" && $position == "director"){
+// if($role == "admin" && $position == "director"){
+if($role == "admin" || $position == "director"){
     echo "<p>管理画面に進んでください。</p>";
 } else {
     echo "<p>あなたは管理者ではないので、ここから先には進めません。</p>";
