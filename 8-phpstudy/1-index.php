18f3c88b85372021775ba22aa6c0e0c59223523b
Soh1121 <satou.shg@gmail.com>
2018-03-27 10:34:17 +0900

    【Arrays 配列の生成と操作】別な生成方法と連想配列を学習
    セクション8 レクチャー108受講中

--- 8-phpstudy/1-index.php
+++ 8-phpstudy/1-index.php
@@ -8,4 +8,16 @@ echo("<br><br>");
 
 echo $myArray[1];
 
+echo("<br><br>");
+
+$anotherArray[0] = "pizza";
+$anotherArray[1] = "chocolate";
+$anotherArray[5] = "coffee";
+
+print_r($anotherArray);
+
+$anotherArray["myFavorite"] = "ice cream";
+
+print_r($anotherArray);
+
 ?>
