d6530297a6ffd905bb77295aa563a8d3a1cb1178
Soh1121 <satou.shg@gmail.com>
2018-03-28 18:44:50 +0900

    【For And For Each Loops】for・foreach 2から30までの整数を表示するプログラム
    セクション8 レクチャー110受講中

--- 8-phpstudy/1-index.php
+++ 8-phpstudy/1-index.php
@@ -1,17 +1,7 @@
 <?php
 
-$role = "admin";
-// $role = "guest";
-
-// $position = "director";
-$position = "manager";
-
-// if($role == "admin"){
-// if($role == "admin" && $position == "director"){
-if($role == "admin" || $position == "director"){
-    echo "<p>管理画面に進んでください。</p>";
-} else {
-    echo "<p>あなたは管理者ではないので、ここから先には進めません。</p>";
+for($i = 2; $i <= 30; $i++){
+    echo $i,"<br>";
 }
 
 ?>
