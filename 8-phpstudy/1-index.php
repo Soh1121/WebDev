7812959e87fb59785c2fa15a1970b04b7b17604d
Soh1121 <satou.shg@gmail.com>
2018-03-28 22:15:11 +0900

    【Sending An Email With PHP（mail関数）】mail関数の利用方法を学習
    セクション8 レクチャー114受講終了

--- 8-phpstudy/1-index.php
+++ 8-phpstudy/1-index.php
@@ -1,13 +1,17 @@
 <?php
 
-// print_r($_GET);
-if($_POST){
-    echo "こんにちは、".$_POST['name']."さん";
+// mail()
+// $emailTo = "rob@percival.com";
+$emailTo = "";
+$subject = "コースの修了証について";
+$body = "修了証の出力方法を教えてください";
+$headers = "";
+
+if(mail($emailTo, $subject, $body, $headers)){
+    echo "送信に成功しました！";
+} else {
+    echo "送信に失敗しました！";
 }
+
 ?>
 
-<p>お名前を入力してください：</p>
-<form method="post">
-    <input type="text" name="name">
-    <input type="submit" value="送信する">
-</form>
