df0288d2fe670dde373f5c030f2d404aafb65893
Soh1121 <satou.shg@gmail.com>
2018-03-28 18:59:50 +0900

    【For And For Each Loops（for・foreach）】配列から順に取り出すプログラム
    セクション8 レクチャー110受講中

--- 8-phpstudy/1-index.php
+++ 8-phpstudy/1-index.php
@@ -1,8 +1,27 @@
 <?php
 
-for($i = 10; $i > 0; $i--){
+for($i = 2; $i < 30; $i++){
     echo $i,",";
 }
-echo $i;
+echo $i."<br><br>";
+
+for($i = 10; $i >= 0; $i--){
+    if($i==0){
+        echo $i;
+    } else {
+        echo $i,",";
+    }
+}
+
+echo "<br><br>";
+
+$family = array("Rob", "Kirsten", "Tommy", "Ralphie");
+for($i = 0; $i < sizeof($family); $i++){
+    if($i == sizeof($family) - 1){
+        echo $family[$i];
+    } else {
+        echo $family[$i],", ";
+    }
+}
 
 ?>
