1641b067f76bd4f019c07aa37011ce4e9db8c5de
Soh1121 <satou.shg@gmail.com>
2018-03-28 21:53:20 +0900

    【Get Variables（GET（フォームとの連携））】URIにクエリを入れた場合
    セクション8 レクチャー112受講中

--- 8-phpstudy/1-index.php
+++ 8-phpstudy/1-index.php
@@ -1,51 +1,5 @@
 <?php
 
-$i = 0;
-while($i < 10){
-    echo $i."<br>";
-    $i++;
-}
-
-$i = 0;
-$family = array("Rob", "Kirsten", "Tommy", "Ralphie");
-while($i < sizeof($family)){
-    echo $family[$i]."Percival, ";
-    $i++;
-}
-
-/*
-for($i = 2; $i < 30; $i++){
-    echo $i,",";
-}
-echo $i."<br><br>";
-
-for($i = 10; $i >= 0; $i--){
-    if($i==0){
-        echo $i;
-    } else {
-        echo $i,",";
-    }
-}
-
-echo "<br><br>";
-
-$family = array("Rob", "Kirsten", "Tommy", "Ralphie");
-for($i = 0; $i < sizeof($family); $i++){
-    if($i == sizeof($family) - 1){
-        echo $family[$i];
-    } else {
-        echo $family[$i],", ";
-    }
-}
-
-echo "<br><br>";
-
-foreach($family as $key => $value){
-    // echo $key.": ".$value.", ";
-    $family[$key] = $value." Percival";
-    echo $family[$key].", ";
-}
-*/
-
+print_r($_GET)
 
 ?>
