9f48726a7dce126acee3a86bd33a49943f8d6c03
Soh1121 <satou.shg@gmail.com>
2018-03-30 18:18:48 +0900

    【データの挿入と削除（データの挿入と更新（INSERTとUPDATE））】データの挿入の学習
    セクション9 レクチャー122受講

--- 9-mysql/index.php
+++ 9-mysql/index.php
@@ -7,6 +7,11 @@
         die("データベースへの接続に失敗しました。");
     }
 
+    $query = "INSERT INTO `users` (`email`, `password`) VALUES ('kirsten@gmail.com', 'thereisnopasswords')";
+    if($result = mysqli_query($link, $query)){
+        echo "INSERTクエリの発行に成功しました。<br>";
+    }
+
     $query = "SELECT * FROM users";
     if($result = mysqli_query($link, $query)){
     //    echo "クエリの発行に成功しました。<br>";
@@ -15,10 +20,11 @@
     // print_r($row);
     // 連想配列型：できればこっち（わかりやすいから）
     echo "<p>あなたのメールアドレスは".$row["email"]."、パスワードは".$row["password"]."です。</p>";
-    
-    // 配列型
+
+
+/*   // 配列型
     echo "<p>あなたのメールアドレスは".$row[1]."、パスワードは".$row[2]."です。</p>";
 
     echo "<p>あなたのIDは".$row["id"]."です。</p>";
-
+*/
 ?>
