285b6976076701d01aa783973bdb40d7f3613b6f
Soh1121 <satou.shg@gmail.com>
2018-03-31 14:38:10 +0900

    【データのループ処理（1/3）（複数レコードの検索取得）】テーブルの内容を全て取得して表示
    セクション9 レクチャー124受講中

--- 9-mysql/index.php
+++ 9-mysql/index.php
@@ -7,26 +7,17 @@
         die("データベースへの接続に失敗しました。");
     }
 
-/*
-    $query = "INSERT INTO `users` (`email`, `password`) VALUES ('kirsten@gmail.com', 'thereisnopasswords')";
-    if($result = mysqli_query($link, $query)){
-        echo "INSERTクエリの発行に成功しました。<br>";
-    }
-*/
-
-    $query = "UPDATE `users` SET `email`='satou.shg@yahoo.co.jp' WHERE `id`=1 LIMIT 1";
-    if($result = mysqli_query($link, $query)){
-        echo "UPDATEクエリの発行に成功しました。<br>";
-    }
-
     $query = "SELECT * FROM users";
     if($result = mysqli_query($link, $query)){
     //    echo "クエリの発行に成功しました。<br>";
     }
-    $row = mysqli_fetch_array($result);
-    // print_r($row);
-    // 連想配列型：できればこっち（わかりやすいから）
-    echo "<p>あなたのメールアドレスは".$row["email"]."、パスワードは".$row["password"]."です。</p>";
+    while($row = mysqli_fetch_array($result)){
+        print_r($row);
+    }
+//    print_r($row);
+//     連想配列型：できればこっち（わかりやすいから）
+
+//    echo "<p>あなたのメールアドレスは".$row["email"]."、パスワードは".$row["password"]."です。</p>";
 
 
 /*   // 配列型
