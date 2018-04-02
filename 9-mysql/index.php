80313cc0e144f0481a86e054cf0d2a5e669ba288
Soh1121 <satou.shg@gmail.com>
2018-04-02 11:18:48 +0900

    【クッキー（クッキーを使ってみよう）】クッキーに値をセットしたり、クリアしたりを学習
    セクション9 レクチャー132受講終了

--- 9-mysql/index.php
+++ 9-mysql/index.php
@@ -1,41 +1,10 @@
 <?php
 
-    session_start();
+// setcookie("customerId", "1234", time() + 60 * 60 * 24 );
+// setcookie("customerId", "", time() - 60 * 60 * 24);
+setcookie("customerId", "test", time() + 60 * 60 * 24);
 
-//    echo $_SESSION['username'];
-    $link = mysqli_connect("localhost", "root", "root", "memberapp");
-    // サーバー名、データベースユーザ名、パスワード
-
-    if(mysqli_connect_error()){
-        die("データベースの接続に失敗しました。");
-    }
-
-//    $query = "SELECT * FROM users WHERE name = '".mysqli_real_escape_string($link, $name)."'";
-
-    if(array_key_exists('email',$_POST) OR array_key_exists('password',$_POST)){
-        // print_r($_POST);
-        if($_POST['email'] == ''){
-            echo "Eメールアドレスを入力してください";
-        } else if($_POST['password'] == '') {
-            echo "パスワードを入力してください";
-        } else {
-            $query = "SELECT `id` FROM `users` WHERE email = '".mysqli_real_escape_string($link, $_POST['email'])."'";
-            $result = mysqli_query($link,$query);
-            if(mysqli_num_rows($result) > 0){
-                echo "既にそのメールアドレスは使用されています。";
-            } else {
-                // 未使用の場合の処理を記述
-                $query = "INSERT INTO `users` (`email`,`password`) VALUES ('".mysqli_real_escape_string($link,$_POST['email'])."','".mysqli_real_escape_string($link,$_POST['password'])."')";
-                if(mysqli_query($link,$query)){
-//                    echo "登録されました！";
-                    $_SESSION['email']=$_POST['email'];
-                    header("Location: session.php");
-                } else {
-                    echo "登録に失敗しました。";
-                }
-            }
-        }
-    }
+echo $_COOKIE["customerId"];
 
 ?>
 
