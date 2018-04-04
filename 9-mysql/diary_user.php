cf6ca566fa5b68f77af678f3916bed32d6b706de
Soh1121 <satou.shg@gmail.com>
2018-04-04 16:45:32 +0900

    【プロジェクト課題：ユーザー登録をして日記を投稿するサイトを作ってみましょう。各ユーザーは自分の投稿した日記だけを参照することができます。ユーザーはメールアドレスとパスワードでサイトに登録します。】ユーザー用のページの投稿フォーム作成。
    セクション9 レクチャー136受講中

--- 9-mysql/diary_user.php
+++ 9-mysql/diary_user.php
@@ -1,3 +1,7 @@
+<?php
+
+?>
+
 <!doctype html>
 <html lang="ja">
     <head>
@@ -8,23 +12,45 @@
         <!-- Bootstrap CSS -->
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
         
+        <style type="text/css">
+            .fix {
+                position: sticky;
+            }
+        </style>
+        
         <title>Diary Service</title>
     </head>
     <body>
         <nav class="navbar navbar-dark bg-primary">
             <a class="navbar-brand" href="#">Diary Service</a>
         </nav>
-        
-        <h1>Hello, world!</h1>
-        
-        <?php
+        <div class="container">
+            <div class="row fix">
+                <div class="col m-sm-2">
+<!--                    1 of 2-->
+                    <form method="post">
+                        <div class="form-group">
+                            <label for="inputTitle" class="m-sm-2">タイトル</label>
+                            <input type="text" class="form-control" id="inputTitle" placeholder="タイトル">
+                            <label for="inputDate" class="m-sm-2">日付</label>
+                            <input type="text" class="form-control" id="inputDate" placeholder="2018年03月02日">
+                            <label for="inputBody" class="m-sm-2">本文</label>
+                            <textarea class="form-control" id="inputBody" rows="5"></textarea>
+                        </div>
+                        <button type="submit" class="btn btn-outline-primary my-1 mx-sm-2" name="post">投稿</button>
+                    </form>
+                </div>
+                <div class="col m-sm-2">
+                    2 of 2
+                </div>
+            </div>
+        </div>
 
-        ?>
-        
         <!-- Optional JavaScript -->
         <!-- jQuery first, then Popper.js, then Bootstrap JS -->
         <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
         <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+        
     </body>
 </html>
