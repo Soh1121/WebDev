2d28495137bf097e000f1f822dd17a1587de18bd
Soh1121 <satou.shg@gmail.com>
2018-05-11 22:27:42 +0900

    【変数とリスト】変数とリスト（タプル・ディクショナリー含む）の学習。型推論など。
    セクション12 レクチャー161受講終了

--- 12-python/hello.py
+++ 12-python/hello.py
@@ -2,4 +2,52 @@
 
 print 'content-type: text/html'
 print ''
-print 'Hello Python!'
+
+age = 35
+
+print age
+
+pi = 3.14
+
+print pi
+
+name = "Rob"
+
+print name
+
+print age / pi
+
+number = "5"
+
+print number * age
+
+print '<br>'
+
+print int(number) * 5
+
+print int(number) * age
+
+print int(number) * pi
+
+str = "My name is Rob"
+print str[0]
+print str[0:6]
+print str[5]
+
+myList = ["Apple", "banana", "orange"]
+print myList
+print myList[1]
+print myList[1:4]
+
+myTuple = (1,2,3,4)
+print myTuple
+
+dict = {}
+dict["dad"] = "Rob"
+dict["mum"] = "Kirsten"
+dict[1] = "Tommy"
+dict[2] = "Raphie"
+print dict
+print dict["mum"]
+print dict.keys()
+print dict.values()
