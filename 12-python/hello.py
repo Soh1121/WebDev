21b766319bd3f0df84b733de2905d062551098b3
Soh1121 <satou.shg@gmail.com>
2018-05-11 22:59:22 +0900

    【If文】pythonでのif文の表記を学習
    セクション12 レクチャー165受講終了

--- 12-python/hello.py
+++ 12-python/hello.py
@@ -3,33 +3,9 @@
 print 'content-type: text/html'
 print ''
 
-for i in range(5, 11):
-    print i
+name = "Rob"
 
-print "Rob"
-
-list = ["pizza", "chocholate", "ice cream"]
-
-# I like eating ***
-
-print "<br>"
-for food in list:
-    print "I like eating " + food + "."
-    print "<br>"
-
-x = 0
-while x <= 10:
-    print x
-    x = x + 1
-    # x += 1
-print "<br>"
-
-ages = {}
-ages["Rob"] = 35
-ages["Kirsteen"] = 36
-ages["Tommy"] = 5
-ages["Raphie"] = 1
-
-for age in ages:
-    print age + " is " + str(ages[age]) + "."
-    
+if name == "Rob" or name == "Kirsten":
+    print "Hello " + name + "!"
+else:
+    print "I don't know."
