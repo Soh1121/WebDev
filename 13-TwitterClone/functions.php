f93baf1ab2adfd848bd191ecab38a2ed0be4a258
Soh1121 <satou.shg@gmail.com>
2018-05-13 20:40:55 +0900

    【最終課題：Twitterクローンを作ってみよう】これまで学習してきた知識を総動員してTwitterクローンを作ってみましょう。１．会員登録・ログインができる　２．ツイートを投稿できる　３．ツイートを表示できる　４．フォローとフォロー解除ができる　５．ユーザーをプロフィールで検索できる。：ログイン・サインアップ機能実装。
    セクション14 レクチャー166受講中

--- 13-TwitterClone/functions.php
+++ 13-TwitterClone/functions.php
@@ -1,6 +1,18 @@
 <?php
   session_start();
+  session_unset();
 
   $link = mysqli_connect("localhost", "root", "root", "twitterclone");
 
+  if(mysqli_connect_errno()){
+    print_r(mysqli_connect_error());
+    exit();
+  }
+/*
+  if(isset($_GET['fuction'])) {
+    if($_GET['function'] == "logout") {
+      session_unset();
+    }
+  }
+*/
 ?>
